6609482eff9ff844f6a6d9caa28b6fbb873033b6
joao <pjoaovictor22@hotmail.com>
2023-05-25 07:43:37 -0300

    at

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const api = axios.create({
-    // baseURL:"https://foodexplorer-efd7.onrender.com"
-    baseURL:'http://localhost:3000'
+    baseURL:"https://foodexplorer-efd7.onrender.com"
+    // baseURL:'http://localhost:3000'
 })
